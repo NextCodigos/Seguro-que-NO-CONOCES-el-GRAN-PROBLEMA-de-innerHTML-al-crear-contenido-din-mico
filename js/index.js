03021a44c8fe11c09d70281e2b1c81e768e6c2f5
NextCodigos <nebulanemesis376@gmail.com>
2024-05-27 14:25:09 +0200

    Problema al crear otro boton no va

---
+++ js/index.js
@@ -0,0 +1,9 @@
+const CAJA=document.querySelector(".caja");
+
+CAJA.innerHTML+=`<button>Aceptar</button>`;
+document.querySelector("button").addEventListener("click",saludar);
+CAJA.innerHTML+=`<button>Cancelar</button>`;
+
+function saludar(){
+       alert("Hola");
+}
