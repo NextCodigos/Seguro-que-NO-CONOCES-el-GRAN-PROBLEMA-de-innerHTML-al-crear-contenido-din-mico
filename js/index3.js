b54d01b21864dc934df6dd4f52b44daa36f61fcb
NextCodigos <nebulanemesis376@gmail.com>
2024-05-27 14:52:41 +0200

    Añado bucle for crear botones con insertAdjacenthtml con afterend arreglado funcionan todos botones

--- js/index3.js
+++ js/index3.js
@@ -6,8 +6,8 @@ for (let k = 0; k < 10; k++) {
 // // Pone fuera de la caja arriba de 1 al 10  
 // CAJA.insertAdjacentHTML("beforebegin", `<button>Boton ${k+1}</button>`);
 // Pone fuera de la caja debajo de 10 al 1
-CAJA.insertAdjacentHTML("afterend", `<button class="bt${k}">Boton ${k+1}</button>`);
-  document.querySelector(".bt"+k).addEventListener("click", saludar);
+CAJA.insertAdjacentHTML("afterend", `<button>Boton ${k+1}</button>`);
+  document.querySelectorAll("button")[k].addEventListener("click", saludar);
 }
 
 function saludar() {
