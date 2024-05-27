f65a7ecace117413ccc731082c38f8aa88c8c471
NextCodigos <nebulanemesis376@gmail.com>
2024-05-27 14:28:06 +0200

    Añado dos botones a misma funciony solo va el de cancelar

--- js/index.js
+++ js/index.js
@@ -2,7 +2,8 @@ const CAJA=document.querySelector(".caja");
 
 CAJA.innerHTML+=`<button>Aceptar</button>`;
 document.querySelector("button").addEventListener("click",saludar);
-// CAJA.innerHTML+=`<button>Cancelar</button>`;
+CAJA.innerHTML+=`<button>Cancelar</button>`;
+document.querySelectorAll("button")[1].addEventListener("click",saludar);
 
 function saludar(){
        alert("Hola");
